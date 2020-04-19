422d0878dea3711d23d2b8ae76b6b47a2adb4d9f
1471104698 <48862368+1471104698@users.noreply.github.com>
2020-04-19 15:28:24 +0800

    Update and rename 1306. 跳跃游戏 III to 1306. 跳跃游戏 III.java

--- 1306. 跳跃游戏 III.java
+++ 1306. 跳跃游戏 III.java
@@ -1,3 +1,4 @@
+/*
 这里有一个非负整数数组 arr，你最开始位于该数组的起始下标 start 处。当你位于下标 i 处时，你可以跳到 i + arr[i] 或者 i - arr[i]。
 
 请你判断自己是否能够跳到对应元素值为 0 的 任意 下标处。
@@ -21,8 +22,8 @@
 输入：arr = [3,0,2,1,2], start = 2
 输出：false
 解释：无法到达值为 0 的下标 1 处。 
+*/
 
-```java
 class Solution {
     public boolean canReach(int[] arr, int start) {
         /*
@@ -43,4 +44,4 @@ class Solution {
         return flag;
     }
 }
-```
+

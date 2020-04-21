b5d0a4a7b7aa89a1819eeefc405cddf195f9c9f7
1471104698 <48862368+1471104698@users.noreply.github.com>
2020-04-21 23:18:06 +0800

    Create 890. 查找和替换模式.java

---
+++ 890. 查找和替换模式.java
@@ -0,0 +1,101 @@
+你有一个单词列表 words 和一个模式  pattern，你想知道 words 中的哪些单词与模式匹配。
+
+如果存在字母的排列 p ，使得将模式中的每个字母 x 替换为 p(x) 之后，我们就得到了所需的单词，那么单词与模式是匹配的。
+
+（回想一下，字母的排列是从字母到字母的双射：每个字母映射到另一个字母，没有两个字母映射到同一个字母。）
+
+返回 words 中与给定模式匹配的单词列表。
+
+你可以按任何顺序返回答案。
+
+ 
+
+示例：
+
+输入：words = ["abc","deq","mee","aqq","dkd","ccc"], pattern = "abb"
+输出：["mee","aqq"]
+解释：
+"mee" 与模式匹配，因为存在排列 {a -> m, b -> e, ...}。
+"ccc" 与模式不匹配，因为 {a -> c, b -> c, ...} 不是排列。
+因为 a 和 b 映射到同一个字母。
+ 
+
+提示：
+
+1 <= words.length <= 50
+1 <= pattern.length = words[i].length <= 20	//每个 word 长度跟 pattern 长度一样，即每个字符分别进行对应
+
+
+下面两种方法，同种思路
+
+//思路①、使用 map 存储 pattern 某个字母对应的字符
+class Solution {
+    public List<String> findAndReplacePattern(String[] words, String pattern) {
+        List<String> res = new ArrayList<>();
+    
+        int len = pattern.length();
+        char[] ps = pattern.toCharArray();
+        for(String word : words){
+            Map<Character, Character> map = new HashMap<>();
+            int i = 0;
+            for(; i < len; i++){
+                char ch = word.charAt(i);
+                if(!map.containsKey(ps[i])){
+                    //如果 value 值存在，那么意味着 word 当前字符已经被别的 pattern 的字符匹配了
+                    if(map.containsValue(ch)){
+                        break;
+                    }
+                    map.put(ps[i], ch);
+                }else if(map.get(ps[i]) != ch){
+                    break;
+                }
+            }
+            if(i == word.length()){
+                res.add(word);
+            }
+        }
+        return res;
+    }
+}
+
+//思路②、使用两个数组代替 map
+class Solution {
+    public List<String> findAndReplacePattern(String[] words, String pattern) {
+        List<String> res = new ArrayList<>();
+        /*
+		不使用 map 了，直接使用两个 26 长度的数组存储 word 和 pattern 的对应情况
+		w 存储 word 某个字符是否已经被对应了
+		p 存储 pattern 某个字符对应的 word 的某个字符
+		
+		如果 p[pNum] == -1 ，那么表示 pattern 当前字符还没有对应，但是如果 w[wNum] == true，表示 word 当前字符已经被别的 pattern 字符对应了，那么 break;
+		否则添加 p[pNum] = wNum，即 设置 pNum 和 wNum 的对应关系，并设置 wNum 已经被对应了
+		
+		如果 p[pNum != -1 ，表示已经对应了某个 word 字符，那么判断对应的字符是否与 word 当前字符相同
+		*/
+
+        int len = pattern.length();
+        for(String word : words){
+            boolean[] w = new boolean[26];
+            int[] p = new int[26];
+            Arrays.fill(p, -1);
+            int i = 0;
+            for(; i < len; i++){
+                int wNum = word.charAt(i) - 'a';
+                int pNum = pattern.charAt(i) - 'a';
+                if(p[pNum] == -1){
+                    if(w[wNum]){
+                        break;
+                    }
+                    p[pNum] = wNum;
+                    w[wNum] = true;
+                }else if(p[pNum] != wNum){
+                    break;
+                }
+            }
+            if(i == len){
+                res.add(word);
+            }
+        }
+        return res;
+    }
+}

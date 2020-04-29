edaf134efcfb9185756315f2d8c6a7ad7af46d1c
1471104698 <48862368+1471104698@users.noreply.github.com>
2020-04-29 16:07:03 +0800

    Create 前缀树模板.java

---
+++ 前缀树/前缀树模板.java
@@ -0,0 +1,29 @@
+    class TrieNode{
+        String val;
+        TrieNode[] childern = new TrieNode[26];
+        记录当前节点是否是某个单词的结尾
+        boolean end = false;
+        public TrieNode() {
+        } 
+
+    class Trie {
+
+        TrieNode root;
+
+        public Trie() {
+            root = new TrieNode();
+        }
+        
+        public void insert(String word) {
+            TrieNode cur = root;
+            for(int i = 0; i  word.length(); i++){
+                int ch = word.charAt(i) - 'a';
+                if(cur.childern[ch] == null){
+                    cur.childern[ch] = new TrieNode();
+                }
+                cur = cur.childern[ch];
+            }
+            cur.end = true;
+            cur.val = word;
+        }
+    }

5fb18685abca564dcff3ab9747aa653ca59a9910
1471104698 <48862368+1471104698@users.noreply.github.com>
2020-05-02 15:10:56 +0800

    Update 3. 无重复字符的最长子串（滑动窗口）.java

--- 双指针（滑动窗口）/3. 无重复字符的最长子串（滑动窗口）.java
+++ 双指针（滑动窗口）/3. 无重复字符的最长子串（滑动窗口）.java
@@ -21,6 +21,9 @@ class Solution {
     public int lengthOfLongestSubstring(String s) {
         /*
         滑动窗口
+		注意：我们只能记录遍历过的字符的个数，如果遇到的当前字符个数存在 1 个，那么我们需要从 left 开始遍历，将遇到的多余的字符都减去，直到在前面的当前字符 ch 为 0
+		我们不能使用记录字符索引位置的方法，然后直接将 left 跳过记录的索引位置的下一个位置，
+		比如 abba，我们刚开始记录 a 位置为 0， b 位置为 1，再次遇到 b，我们会将 left 跳到 1 后面的字符，那么这时候， left = 2, right = 3，但是再次遇到 a，left 会跳到记录的 a 的位置的下一个位置，即 1，导致结果错误
         */
         int[] have = new int[127];
 

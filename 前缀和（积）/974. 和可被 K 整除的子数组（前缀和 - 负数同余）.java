f0c922c7bdb187d61905ec0954fd09033b4a7391
1471104698 <48862368+1471104698@users.noreply.github.com>
2020-05-15 09:55:55 +0800

    Update 974. 和可被 K 整除的子数组（前缀和 - 负数同余）.java

--- 前缀和（积）/974. 和可被 K 整除的子数组（前缀和 - 负数同余）.java
+++ 前缀和（积）/974. 和可被 K 整除的子数组（前缀和 - 负数同余）.java
@@ -7,45 +7,37 @@
 输入：A = [4,5,0,-2,-3,1], K = 5
 输出：7
 解释：
-有 7 个子数组满足其元素之和可被 K = 5 整除：
-[4, 5, 0, -2, -3, 1], [5], [5, 0], [5, 0, -2, -3], [0], [0, -2, -3], [-2, -3]
- 
-
-提示：
-
-1 <= A.length <= 30000
--10000 <= A[i] <= 10000
-2 <= K <= 10000
 
 class Solution {
     public int subarraysDivByK(int[] A, int K) {
         /*
-        (preSum[i] − preSum[j]) mod k == 0 ⟺ preSum[i] mod k == preSum[j] mod k
+        (preSum[i] - preSum[j]) mod k == 0  ⟺ preSum[i] mod k == preSum[j] mod k
 
-        我们可以使用 map 记录 preSum[i] 的个数
 
-        4,5,0,-2,-3,1
-      0 4 9 9   
+        如果 sum 为正数，那么直接 mod k 即可
+        如果 sum 为负数，那么需要转换为转换为正数，
+            比如 sum = -1,根据同余定理： 如果 k = 6, 那么 -1 的余数为 5， 即 (-1 = 5) mod 6，则可转换为 sum += k = -1 + 6 = 5
+            比如 sum = -7， 我们可以先 mod k, 再 + k, 即 sum = sum mod k + k = (-7 mod 6) + 6 = -1 + 6 = 5
+        综上，我们只需要 
+        1、sum += num;
+        2、(sum % K + K) % K
         */
+		
+		//这里我们可以使用 map，但题目说了 -10000 <= A[i] <= 10000，2 <= K <= 10000， 那么最终 sum 的范围为 [0, 10000]，因为负数也会转换为整数
+		//因此我们只需要开 10001 大小的数组存放数据即可
+        int[] arr = new int[10001];
 
-        Map<Integer, Integer> map = new HashMap<>();
-        map.put(0, 1);
-        int c = 0;
+        arr[0] = 1;
+
+        int res = 0;
+
         int sum = 0;
         for(int num : A){
-            /* 
-            这里不直接用 sum[i] % K，是因为 sum[i] 会有负数，根据同余定理：
-            比如 len = 6 ，
-            num = -2 那么根据同余 它的余数为 4
-            相当于 num % len + len = -2 % 6 + 6 = -2 + 6 = 4
-            */
-            //负数同余，这里先取 % 是因为可能 num = -10000，而 K = 10,所以需要先取余缩减到 -[0, K]
-            num = num % K + K;
-            sum = (sum + num) % K;
-            int time = map.getOrDefault(sum, 0);
-            c += time;
-            map.put(sum, time + 1);
+            sum += num;
+            sum = (sum % K + K) % K;
+            res += arr[sum];
+           arr[sum]++;
         }
-        return c;
-    }
+        return res;
+    }   
 }
